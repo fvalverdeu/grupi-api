f9675abc30135d43003a9ef3a0af5ee88e2f63dc
fvalverdeu <fvalverdeu@gmail.com>
2023-10-30 01:25:36 -0500

    feat(grupi): add auth-token validation in socket

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
+const config_1 = __importDefault(require("./config/config"));
 require("./database");
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 // Socket Server
 const server = require('http').createServer(app_1.default);
 const io = require('socket.io')(server);
 // Socket messages
 io.on('connection', (client) => {
     console.log('Cliente conectado');
+    console.log(client.handshake.headers['auth-token']);
+    const token = client.handshake.headers['auth-token'];
+    if (!token) {
+        return client.disconnect();
+    }
+    const id = jsonwebtoken_1.default.verify(token, config_1.default.jwtSecret);
+    console.log('SOCKE ID USER', id);
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from "./app";
+import config from "./config/config";
 import "./database";
+import jwt, { decode } from 'jsonwebtoken';
 
 // Socket Server
 const server = require('http').createServer(app);
@@ -8,6 +10,11 @@ const io = require('socket.io')(server);
 // Socket messages
 io.on('connection', (client: any) => {
     console.log('Cliente conectado');
+    console.log(client.handshake.headers['auth-token']);
+    const token = client.handshake.headers['auth-token'];
+    if (!token) { return client.disconnect(); }
+    const id = jwt.verify(token, config.jwtSecret);
+    console.log('SOCKE ID USER', id);
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
